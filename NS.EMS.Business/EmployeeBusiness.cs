179db6b9abef7e492f80a080401f563b3df32b96
Ankita Lande <97441450+Ankita-Lande17@users.noreply.github.com>
2022-02-17 14:04:45 +0530

    Add files via upload

---
+++ NS.EMS.Business/EmployeeBusiness.cs
@@ -0,0 +1,50 @@
+﻿using NS.EMS.Data.Entities;
+using NS.EMS.Model;
+using NS.EMS.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NS.EMS.Business
+{
+    public class EmployeeBusiness : IEmployeeBusiness
+    {
+        private readonly IEmployeeRepo _IEmployeeRepo;
+        public EmployeeBusiness(IEmployeeRepo IEmployeeRepo)
+        {
+            _IEmployeeRepo = IEmployeeRepo;
+        }
+
+        public bool AddEmployee(EmployeeDtModel EmpDept)
+        {
+            return _IEmployeeRepo.AddEmployee(EmpDept);
+        }
+        public List<EmployeeDtModel> ShowDetail()
+        {
+
+            return _IEmployeeRepo.ShowDetail();
+
+        }
+
+
+        public List<EmployeeDtModel> UpdateEmployee(int id)
+        {
+            return _IEmployeeRepo.UpdateEmployee(id);
+        }
+
+        public bool FinalUpdate(EmployeeDtModel EmpDept)
+        {
+            return _IEmployeeRepo.FinalUpdate(EmpDept);
+        }
+
+        public List<EmployeeDtModel> DeleteEmployee(int id)
+        {
+            return _IEmployeeRepo.DeleteEmployee(id);
+        }
+
+        public bool FinalDelete(EmployeeDtModel EmpDept)
+        {
+            return _IEmployeeRepo.FinalDelete(EmpDept);
+        }
+    }
+}
---
+++ NS.EMS.Business/IEmployeeBusiness.cs
@@ -0,0 +1,19 @@
+﻿using NS.EMS.Data.Entities;
+using NS.EMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NS.EMS.Business
+{
+    public interface IEmployeeBusiness
+    {
+        bool AddEmployee(EmployeeDtModel EmpDept);
+
+        List<EmployeeDtModel> ShowDetail();
+        List<EmployeeDtModel> UpdateEmployee(int id);
+        bool FinalUpdate(EmployeeDtModel EmpDept);
+        List<EmployeeDtModel> DeleteEmployee(int id);
+        bool FinalDelete(EmployeeDtModel EmpDept);
+    }
+}
