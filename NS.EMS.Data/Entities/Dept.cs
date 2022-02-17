9615058f3e148d1cdbbb950dcca89a8e37f68224
Ankita Lande <97441450+Ankita-Lande17@users.noreply.github.com>
2022-02-17 13:59:35 +0530

    Add files via upload

---
+++ NS.EMS.Data/Entities/Dept.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NS.EMS.Data.Entities
+{
+    public partial class Dept
+    {
+        [Required]
+        public int DeptId { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string DeptName { get; set; }
+    }
+}
---
+++ NS.EMS.Data/Entities/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NS.EMS.Data.Entities
+{
+    public partial class Employee
+    {
+        [Required]
+        public int Eid { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Mobile { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public int? DeptId { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Gender { get; set; }
+    }
+}
